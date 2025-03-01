34d065b087aa4877c5a0481c751f4943e0eb4d35
jwalters-code <jadedinparadise@gmail.com>
2025-02-28 19:40:23 -0700

    created 2 new classes, added instrictions for HR Part 2
    new classes are PersonImperialSet and PersonOrderedSet

---
+++ PersonOrderedSet.java
@@ -0,0 +1,18 @@
+/*
+Jade Walters
+CSCI 2251
+Assignment: Human Resources
+Purpose: to practice creating classes and using inheritence
+Sources: I referenced some of my past assignments such as 
+HurricaneRowData and TicTacToe
+--I watched a Coding with John tutorial about interfaces and abstract class
+source: https://www.youtube.com/watch?v=HvPlEJ3LHgE
+--I looked at a GeeksforGeeks page for help on parseDouble method
+source: https://www.geeksforgeeks.org/convert-string-to-double-in-java/
+--I got help on the toString method for array lists from PrepInsta
+source: https://prepinsta.com/java/arraylist-tostring-method/
+*/
+
+public class PersonOrderedSet extends PersonSet (
+
+}
